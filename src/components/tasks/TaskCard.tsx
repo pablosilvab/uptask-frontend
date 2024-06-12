98c2c16074694a9b0ee3ff533aa10497fa01b02d
Pablo Silva <pablonicolassilvabravo@gmail.com>
2024-06-12 17:56:01 -0400

    showing task information with modal

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -40,6 +40,7 @@ export default function TaskCard({ task }: TaskCardProps) {
     <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3 ">
       <div className="min-w-0 flex flex-col gap-y-4">
         <button
+          onClick={() => navigate(location.pathname + `?viewTask=${task._id}`)}
           type="button"
           className="text-xl font-bold text-slate-600 text-left"
         >
@@ -65,6 +66,9 @@ export default function TaskCard({ task }: TaskCardProps) {
             <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
               <MenuItem>
                 <button
+                  onClick={() =>
+                    navigate(location.pathname + `?viewTask=${task._id}`)
+                  }
                   type="button"
                   className="block px-3 py-1 text-sm leading-6 text-gray-900"
                 >
---
+++ src/components/tasks/TaskModalDetails.tsx
@@ -0,0 +1,103 @@
+import { Fragment } from "react";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getTaskById } from "@/api/taskAPI";
+import { toast } from "react-toastify";
+import { formatDate } from "@/utils/utils";
+
+export default function TaskModalDetails() {
+  const navigate = useNavigate();
+
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const taskId = queryParams.get("viewTask")!;
+
+  const show = taskId ? true : false;
+
+  const { data, isError, error } = useQuery({
+    queryKey: ["task", taskId],
+    queryFn: () => getTaskById({ projectId, taskId }),
+    enabled: !!taskId,
+    retry: false,
+  });
+
+  if (isError) {
+    toast.error(error.message, { toastId: "error" });
+    return <Navigate to={`/projects/${projectId}`} />;
+  }
+
+  if (data)
+    return (
+      <>
+        <Transition appear show={show} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-10"
+            onClose={() => navigate(location.pathname, { replace: true })}
+          >
+            <TransitionChild
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black/60" />
+            </TransitionChild>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <TransitionChild
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
+                    <p className="text-sm text-slate-400">
+                      Agregada el: {formatDate(data.createdAt)}
+                    </p>
+                    <p className="text-sm text-slate-400">
+                      Última actualización: {formatDate(data.updatedAt)}
+                    </p>
+                    <DialogTitle
+                      as="h3"
+                      className="font-black text-4xl text-slate-600 my-5"
+                    >
+                      {data.name}
+                    </DialogTitle>
+                    <p className="text-lg text-slate-500 mb-2">
+                      Descripción: {data.description}
+                    </p>
+                    <div className="my-5 space-y-3">
+                      <label className="font-bold">Estado Actual: </label>
+                    </div>
+                  </DialogPanel>
+                </TransitionChild>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
+      </>
+    );
+}
