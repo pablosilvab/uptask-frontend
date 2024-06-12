4643ccb7e6b867b20c98e521cc742d973a90d6fc
Pablo Silva <pablonicolassilvabravo@gmail.com>
2024-06-12 09:52:05 -0400

    add view for project details view and modal for tasks

---
+++ src/views/projects/ProjectDetailsView.tsx
@@ -0,0 +1,41 @@
+import { getProjectById } from "@/api/projectAPI";
+import AddTaskModal from "@/components/tasks/AddTaskModal";
+import { useQuery } from "@tanstack/react-query";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+
+export default function ProjectDetailsView() {
+  const navigate = useNavigate();
+
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["editProject", projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: 2,
+  });
+
+  if (isLoading) return "Cargando...";
+  if (isError) return <Navigate to="/404" />;
+
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">{data.projectName}</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          {data.description}
+        </p>
+        <nav className="my-5 flex gap-3">
+          <button
+            type="button"
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            onClick={() => navigate(location.pathname + "?newTask=true")}
+          >
+            Agregar tarea
+          </button>
+        </nav>
+
+        <AddTaskModal />
+      </>
+    );
+}
