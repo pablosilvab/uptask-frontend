3a4739ed3416797f58fece299b77f7e61c7e6175
Pablo Silva <pablonicolassilvabravo@gmail.com>
2024-06-16 01:49:52 -0400

    logout and navigate fix

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -3,12 +3,23 @@ import { Popover, PopoverButton, Transition } from "@headlessui/react";
 import { Bars3Icon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 import { User } from "../types";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 type NavMenuProps = {
   name: User["name"];
 };
 
 export default function NavMenu({ name }: NavMenuProps) {
+  const queryClient = useQueryClient();
+  const logout = () => {
+    localStorage.removeItem("AUTH_TOKEN");
+    queryClient.invalidateQueries({
+      queryKey: ["user"],
+    });
+    toast.success("Cerrando sesión..")
+  };
+
   return (
     <Popover className="relative">
       <PopoverButton className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400">
@@ -36,7 +47,7 @@ export default function NavMenu({ name }: NavMenuProps) {
             <button
               className="block p-2 hover:text-purple-950"
               type="button"
-              onClick={() => {}}
+              onClick={logout}
             >
               Cerrar Sesión
             </button>
--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,12 +1,14 @@
 import { useForm } from "react-hook-form";
 import { UserLoginForm } from "@/types/index";
 import ErrorMessage from "@/components/ErrorMessage";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { authenticateUser } from "@/api/authAPI";
 import { toast } from "react-toastify";
 
 export default function LoginView() {
+  const navigate = useNavigate();
+
   const initialValues: UserLoginForm = {
     email: "",
     password: "",
@@ -22,8 +24,8 @@ export default function LoginView() {
     onError: (error) => {
       toast.error(error.message);
     },
-    onSuccess: (data) => {
-      toast.success(data.message);
+    onSuccess: () => {
+      navigate("/");
     },
   });
 
