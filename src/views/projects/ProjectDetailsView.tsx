42b3c7d09a87725bcb2e5200d554a7ee3af969b1
Pablo Silva <pablonicolassilvabravo@gmail.com>
2024-06-17 10:37:49 -0400

    add return to projects button

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -29,6 +29,13 @@ export default function ProjectDetailsView() {
           {data.description}
         </p>
         <nav className="my-5 flex gap-3">
+          <Link
+            to={"/"}
+            className="bg-fuchsia-600 hover:bg-fuchsia-700 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          >
+            <span>Proyectos</span>
+          </Link>
+
           <button
             type="button"
             className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
